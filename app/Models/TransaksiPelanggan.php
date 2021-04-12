cbb3537c5ef7be6329572293925a2b5d3513addc
wendiNurhermansah <wendii.2mia3@gmail.com>
2021-04-12 10:56:00 +0700

    dikit

--- app/Models/TransaksiPelanggan.php
+++ app/Models/TransaksiPelanggan.php
@@ -7,15 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class TransaksiPelanggan extends Model
 {
     protected $table = 'tmtransaksi_pelanggan';
-<<<<<<< HEAD
-    protected $fillable = ['pelanggan_id','total','diskon','pajak','dibayar','catatan','created_at','update_at'];
-
-
-=======
     protected $fillable = ['pelanggan_id','total','diskon','pajak','dibayar','metode','catatan','created_at','update_at'];
 
-    public function Pelangan(){
+    public function Pelanggan(){
         return $this->belongsTo(Pelanggan::class, 'pelanggan_id');
     }
->>>>>>> 5e42751d2eb4da6070e25f8fd80d6d2d046e1d51
+
 }
