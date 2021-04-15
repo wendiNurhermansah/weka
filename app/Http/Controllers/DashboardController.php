c6ceab908f3a6b00ddb11351f525218de6c81faa
wendiNurhermansah <wendii.2mia3@gmail.com>
2021-04-15 09:03:58 +0700

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,13 +17,13 @@ class DashboardController extends Controller
         $categoryTotal = DB::select('SELECT produks.nama,tmtransaksi_pelanggan_detail.produk_id,count( tmtransaksi_pelanggan_detail.kuantitas ) AS total FROM tmtransaksi_pelanggan_detail JOIN produks ON produks.id = tmtransaksi_pelanggan_detail.produk_id WHERE MONTH ( tmtransaksi_pelanggan_detail.created_at ) = '.$bulan.' GROUP BY tmtransaksi_pelanggan_detail.produk_id');
 
         // dd($categoryTotal);
-        $categories = [];
-        $data = [];
+        $categories1 = [];
+        $data1 = [];
 
         foreach($categoryTotal as $i){
-            $categories [] = $i->nama;
-            $data [] = $i->total;
+            $categories1 [] = $i->nama;
+            $data1 [] = $i->total;
         }
-        return view('dashboard', compact('categories', 'data')) ;
+        return view('dashboard', compact('categories1', 'data')) ;
     }
 }
